bc2d3a0e79f89be273a22f8fcdbfbc72a8763cfa
noTformaT <noTformaT@gmail.com>
2023-02-28 02:58:48 +0200

    Add Test project

---
+++ Test/Source/Utilities.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <fstream>
+
+const int MAX_FRAME_DRAWS = 3;
+
+const std::vector<const char*> deviceExtensions = {
+	VK_KHR_SWAPCHAIN_EXTENSION_NAME
+};
+
+// Indieces (locations) of Queue Families (if they exist at all)
+struct QueueFamilyIndices
+{
+	int graphicsFamily = -1;			// Locations of Graphics Queue Family
+	int presentationFamily = -1;		// Location of Presentation Queue Family
+
+	// Check if queue family is valid
+	bool isValid()
+	{
+		return graphicsFamily >= 0 && presentationFamily >= 0;
+	}
+
+};
+
+struct SwapChainDetails
+{
+	VkSurfaceCapabilitiesKHR surfaceCapabilities;		// Surface properies, e. g. image size, extent...
+	std::vector<VkSurfaceFormatKHR> formats;			// Surface image formats, e. g. RGBA and size of each colout
+	std::vector<VkPresentModeKHR> presentationModes;	// How images should be presented to screen
+
+	// Valid if formats > 0 and presentations modes > 0
+	bool isValid()
+	{
+		return !formats.empty() && !presentationModes.empty();
+	}
+};
+
+// SwapChain image struct
+struct SwapChainImage
+{
+	VkImage image;
+	VkImageView imageView;
+};
+
+static std::vector<char> readFile(const std::string& fileName)
+{
+	// open stream from given file
+	// std::ios::binary tells stream to read file as binary
+	// std::ios::ate tells stream to start reading from end of file
+	std::ifstream file(fileName, std::ios::binary | std::ios::ate);
+
+	printf("Read binary file: %s\n", fileName.c_str());
+
+	// Check if file stream successfully open
+	if (!file.is_open())
+	{
+		throw std::runtime_error("Failed to open a file!");
+	}
+
+	// Get current read position to resize file buffer
+	size_t fileSize = (size_t)file.tellg();
+	std::vector<char> fileBuffer(fileSize);
+
+	// Move read position (seek to) the start of the file
+	file.seekg(0);
+
+	// Read the file data into the buffer (stream "fileSize" in total)
+	file.read(fileBuffer.data(), fileSize);
+
+	file.close();
+
+	return fileBuffer;
+}
---
+++ Test/Source/VulkanRenderer.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <stdexcept>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <array>
+
+#include "VulkanValidation.h"
+#include "Utilities.h"
+
+class VulkanRenderer
+{
+public:
+	VulkanRenderer();
+
+	int init(GLFWwindow* newWindow);
+	void draw();
+	void cleanup();
+
+	~VulkanRenderer();
+
+private:
+	GLFWwindow* window;
+
+	int currentFrame = 0;
+
+	// Vulkan Components
+	// - Main
+	VkInstance instance;
+	VkDebugReportCallbackEXT callback;
+	struct {
+		VkPhysicalDevice physicalDevice;
+		VkDevice logicalDevice;
+	} mainDevice;
+	VkQueue graphicsQueue;
+	VkQueue presentationQueue;
+	VkSurfaceKHR surface;
+	VkSwapchainKHR swapchain;
+
+	std::vector<SwapChainImage> swapChainImages;
+	std::vector<VkFramebuffer> swapChainFramebuffers;
+	std::vector<VkCommandBuffer> commandBuffers;
+
+	// - Pipeline
+	VkPipeline graphicsPipeline;
+	VkPipelineLayout pipelineLayout;
+	VkRenderPass renderPass;
+
+	// - Pools
+	VkCommandPool graphicsCommandPool;
+
+	// - Utility
+	VkFormat swapChainImageFormat;
+	VkExtent2D swapChainExtent;
+
+	// - Synchronisation
+	std::vector<VkSemaphore> imageAvailable;
+	std::vector<VkSemaphore> renderFinished;
+	std::vector<VkFence> drawFences;
+
+	// Vulkan Functions
+	// - Create Functions
+	void createInstance();
+	void createDebugCallback();
+	void createLogicalDevice();
+	void createSurface();
+	void createSwapChain();
+	void createRenderPass();
+	void createGraphicsPipeline();
+	void createFramebuffers();
+	void createCommandPool();
+	void createCommandBuffers();
+	void createSynchronisation();
+
+	// - Record Functions
+	void recordCommands();
+
+	// - Get Functions
+	void getPhysicalDevice();
+
+	// - Support Functions
+	// -- Checker Functions
+	bool checkInstanceExtensionSupport(std::vector<const char*>* checkExtensions);
+	bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+	bool checkValidationLayerSupport();
+	bool checkDeviceSuitable(VkPhysicalDevice device);
+
+	// -- Getter Functions
+	QueueFamilyIndices getQueueFamilies(VkPhysicalDevice device);
+	SwapChainDetails getSwapChainDetails(VkPhysicalDevice device);
+
+	// -- Choose Functions
+	VkSurfaceFormatKHR chooseBestSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& formats);
+	VkPresentModeKHR chooseBestPresentationMode(const std::vector<VkPresentModeKHR> presentationModes);
+	VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& surfaceCapabilities);
+
+	// -- Create Functions
+	VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags);
+	VkShaderModule createShaderModule(const std::vector<char>& code);
+
+};
+
