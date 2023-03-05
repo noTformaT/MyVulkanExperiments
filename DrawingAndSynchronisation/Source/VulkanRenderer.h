11690df893b8cc62899aaa4d60237c038dccf8d5
noTformaT <noTformaT@gmail.com>
2023-03-05 17:05:51 +0200

    Add Drawing And Synchronisation Sample

---
+++ DrawingAndSynchronisation/Source/VulkanRenderer.h
@@ -0,0 +1,106 @@
+#pragma once
+
+//#define VK_USE_PLATFORM_WIN32_KHR
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <stdexcept>
+#include <vector>
+#include <set>
+#include <array>
+
+#include "VulkanValidation.h"
+#include "Utilities.h"
+
+class VulkanRenderer
+{
+public:
+	VulkanRenderer();
+
+	int Init(GLFWwindow* newWindow);
+	void Draw();
+	void Cleanup();
+
+	~VulkanRenderer();
+
+private:
+	GLFWwindow* window = nullptr;
+
+	int currentFrame = 0;
+
+	// Vulkan components
+	// - Main
+	VkInstance instance = nullptr;
+	VkDebugReportCallbackEXT callback;
+
+	struct
+	{
+		VkPhysicalDevice physicalDevice;
+		VkDevice logicalDevice;
+	} mainDevice;
+
+	VkQueue graphicsQueue;
+	VkQueue presentationQueue;
+	VkSurfaceKHR surface;
+	VkSwapchainKHR swapChain;
+	std::vector<SwapChainImage> swapChainImages;
+	std::vector<VkFramebuffer>	swapChainFrameBuffers;
+	std::vector<VkCommandBuffer> commandBuffers;
+
+	// - Pipeline
+	VkPipeline graphicsPipeline;
+	VkPipelineLayout pipelineLayout;
+	VkRenderPass renderPass;
+
+	// - Pools
+	VkCommandPool graphicsCommandPool;
+
+	// - Utility
+	VkFormat swapChainImageFormat;
+	VkExtent2D swapChainExtent;
+
+	// - Synchronisation
+	std::vector<VkSemaphore> imageAvaible;
+	std::vector<VkSemaphore> renderFinished;
+	std::vector<VkFence> drawFences;
+
+	// Vulkan functions
+	// - Create functions
+	void CreateInstance();
+	void CreateDebugCallback();
+	void CreateLogicalDevice();
+	void CreateSurface();
+	void CreateSwapChain();
+	void CreateRenderPass();
+	void CreateGraphicsPipeline();
+	void CreateFrameBuffers();
+	void CreateCommandPool();
+	void CreateCommandBuffers();
+	void CreateSynchronisation();
+
+	// - Record Functions
+	void RecordCommands();
+
+	// - Get functions
+	void GetPhysicalDevice();
+
+	// - Support functions
+	// -- Checker Functions
+	bool CheckInstanceExtensionsSupport(std::vector<const char*>* checkExtensions);
+	bool CheckDeviceExtensionsSupport(VkPhysicalDevice device);
+	bool CheckValidationLayerSupport();
+	bool CheckDeviceSuitable(VkPhysicalDevice device);
+
+	// -- Getter Functions
+	QueueFamilyIndices GetQueueFamilies(VkPhysicalDevice device);
+	SwapChainDetails GetSwapChainDetails(VkPhysicalDevice device);
+
+	// --Choose Formats
+	VkSurfaceFormatKHR ChooseBestSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& formats);
+	VkPresentModeKHR ChooseBestPresentationMode(const std::vector<VkPresentModeKHR>& presentaionModes);
+	VkExtent2D ChooseSwapExtent(const VkSurfaceCapabilitiesKHR& surfaceCapabilities);
+
+	// --Create functions
+	VkImageView CreateImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags);
+	VkShaderModule CreateShaderModule(const std::vector<char>& code);
+};
